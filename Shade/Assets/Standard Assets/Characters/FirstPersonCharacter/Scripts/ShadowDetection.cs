bc80063e8cec2343aed2b3172be0427e9618074f
Ryan Torres <ryanmt@uci.edu>
2015-12-30 22:23:49 -0800

    Ironing out more issues with the shadow detection system.

--- Shade/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/ShadowDetection.cs
+++ Shade/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/ShadowDetection.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 public class ShadowDetection : MonoBehaviour
 {
     public Image shadowDetector;
-    int lightRange;
+    float lightMeter;
+    float lightRange;
     
 	// Use this for initialization
 	void Start()
     {
-	    
-	}
+        lightRange = 0;
+        lightMeter = 0;
+        shadowDetector.color = new Color(lightMeter, lightMeter, lightMeter, 1);
+    }
 	
 	// Update is called once per frame
 	void Update()
@@ -23,8 +26,28 @@ public class ShadowDetection : MonoBehaviour
     {
         if (coll.tag == "Light")
         {
-            Debug.Log("Hello!");
+            StartCoroutine("ShadowCoroutine", coll);
         }
+    }
+
+    IEnumerator ShadowCoroutine(Collider coll)
+    {
+        yield return new WaitForSeconds(0.1f);
+        if(distanceToLight(coll) < coll.GetComponent<Light>().range)
+        {
+            //Shadow Calculations
+            lightRange = distanceToLight(coll);
+            lightMeter = 1 - (lightRange / coll.GetComponent<Light>().range);
+            shadowDetector.color = new Color(lightMeter, lightMeter, lightMeter, 1);
+            //Restart Coroutine if you are still in light.
+            Debug.Log("Hello! Your light is at:" + lightMeter);
+            StartCoroutine("ShadowCoroutine", coll);
+        }
+    }
+
+    float distanceToLight(Collider coll)
+    {
+        return Mathf.Sqrt(Mathf.Pow((gameObject.transform.position.x - coll.transform.position.x), 2) + Mathf.Pow((gameObject.transform.position.y - coll.transform.position.y), 2) + Mathf.Pow((gameObject.transform.position.z - coll.transform.position.z), 2));
     }
 
     void OnTriggerExit(Collider coll)
